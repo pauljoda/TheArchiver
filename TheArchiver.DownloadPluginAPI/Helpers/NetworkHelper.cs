616d4ab7facabb7187a5ecf973629223b0299dc8
Paul Davis <pauldavis101@gmail.com>
2025-01-07 19:23:36 -0600

    Fix namespace typo and add new helper methods.
    Corrected "TheArhiver" to "TheArchiver" in multiple files to fix a namespace typo. Added methods for downloading files, processing cookies, and fetching website content with cookies in helper classes to expand functionality. Updated README and project files to reflect these changes.

--- TheArchiver.DownloadPluginAPI/Helpers/NetworkHelper.cs
+++ TheArchiver.DownloadPluginAPI/Helpers/NetworkHelper.cs
@@ -1,8 +1,14 @@
 ﻿using System.Net;
 
-namespace TheArhiver.DownloadPluginAPI.Helpers;
+namespace TheArchiver.DownloadPluginAPI.Helpers;
 
 public class NetworkHelper {
+    
+    /// <summary>
+    /// Retrieves the local IPv4 address of the machine.
+    /// </summary>
+    /// <returns>The local IPv4 address as a string.</returns>
+    /// <exception cref="Exception">Thrown when a local IPv4 address cannot be found.</exception>
     public static string GetLocalIpAddress() {
         var host = Dns.GetHostEntry(Dns.GetHostName());
         foreach (var ip in host.AddressList) {
